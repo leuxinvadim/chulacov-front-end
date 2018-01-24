acc8a391f56fb1ce8372ca5ce834965231572c7f
Alexandr Bondarenko <bondalex96@gmail.com>
2018-01-24 23:20:58 +0300

    Поле для ввода имени держателя карты теперь валидируется

--- script.js
+++ script.js
@@ -9,28 +9,18 @@ $('body').on('click keypress', '.enter', function(key) {
         return false;
 });
 
-$(function() {
-    jQuery(function($) {
-        $('#cyr').on('keypress', function() {
+// Удаляет символы, отличные от латиницы и пробелов
+function OnInputOwner(input) {
+    input.value = input.value.replace(/[^A-z\s]/g, '');
+}
+// Проверяет длину введенных данных
+function OnChangeOwner(input) {
+    var maxLen = 3;
+    if (input.value.length < maxLen)
+        return alert(`Введите больше ${maxLen} символов`);
+}
 
-            var that = this;
-            setTimeout(function() {
-                var res = /[^A-z ]/g.exec(that.value);
-                that.value = that.value.replace(res, '');
-            }, 0);
-
-
-            $this = $(this);
-            $maxLen = 3;
-            // ограничение по длине
-            if ($this.val().length < $maxLen)
-                return alert('Введите больше 3х символов');
-
-        });
-    });
-});
-
-jQuery('body').on('click keypress', '.code_CVC', function(key) {
+$('body').on('click keypress', '.code_CVC', function(key) {
     $this = $(this);
     $maxLen = 2;
     // ограничение по длине
