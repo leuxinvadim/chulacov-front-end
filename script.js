c66a07d0e48d50a35b21cbb4c90a99f3fcd19355
Vadim Leukhin <leuxinvadim@gmail.com>
2018-01-24 19:41:26 +0300

    работа над третьим заданием

--- script.js
+++ script.js
@@ -4,8 +4,37 @@ $('body').on('click keypress', '.enter', function(key) {
     // ограничение по длине
     if ($this.val().length > $maxLen)
         return false;
-    // ограничение по вводу нуля в начале
-    if ($this.val().length == 0 && key.charCode == 48)
+    // ограничение по символам
+    if (key.charCode < 48 || key.charCode > 57)
+        return false;
+});
+
+$(function() {
+    jQuery(function($) {
+        $('#cyr').on('keypress', function() {
+
+            var that = this;
+            setTimeout(function() {
+                var res = /[^A-z ]/g.exec(that.value);
+                that.value = that.value.replace(res, '');
+            }, 0);
+
+
+            $this = $(this);
+            $maxLen = 3;
+            // ограничение по длине
+            if ($this.val().length < $maxLen)
+                return alert('Введите больше 3х символов');
+
+        });
+    });
+});
+
+jQuery('body').on('click keypress', '.code_CVC', function(key) {
+    $this = $(this);
+    $maxLen = 2;
+    // ограничение по длине
+    if ($this.val().length > $maxLen)
         return false;
     // ограничение по символам
     if (key.charCode < 48 || key.charCode > 57)
