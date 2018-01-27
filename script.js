07323537715f25c637a877f119dd6ab8851a69aa
Alexandr Bondarenko <bondalex96@gmail.com>
2018-01-27 15:41:14 +0300

    Поправлены обработчики инпутов

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-$('body').on('click keypress', '.enter', function(key) {
-    $this = $(this);
-    $maxLen = 3;
-    // ограничение по длине
-    if ($this.val().length > $maxLen)
-        return false;
-    // ограничение по символам
-    if (key.charCode < 48 || key.charCode > 57)
-        return false;
-});
+function cutCharactersExceptNumbers(input, maxCharacters) {
+    input.value = input.value.replace(/[^\d]/g, '');
+    input.value = input.value.slice(0, maxCharacters);
+}
 
 // Удаляет символы, отличные от латиницы и пробелов
 function OnInputOwner(input) {
     input.value = input.value.replace(/[^A-z\s]/g, '');
 }
+
 // Проверяет длину введенных данных
 function OnChangeOwner(input) {
     var maxLen = 3;
@@ -27,56 +22,46 @@ function OnChangeCVC(input) {
         return alert('Введите 3 символа');
 }
 
-$('body').on('click keypress', '.code_CVC', function(key) {
-    $this = $(this);
-    $maxLen = 2;
-    // ограничение по длине
-    if ($this.val().length > $maxLen)
-        return false;
-    // ограничение по символам
-    if (key.charCode < 48 || key.charCode > 57)
-        return false;
-});
+function sendform(form) {
 
+    console.log(form);
 
-function sendform() {
+      if (document.forms[0].fname.value == "") {
+          alert('Пожалуйста, введите Номер карты');
+          document.card_form.fname.focus();
+          return false
+      }
 
-    if (document.forms[0].fname.value == "") {
-        alert('Пожалуйста, введите Номер карты');
-        document.card_form.fname.focus();
-        return false
-    }
+      if (document.forms[0].fname_1.value == "") {
+          alert('Пожалуйста, введите Номер карты');
+          document.card_form.fname_1.focus();
+          return false
+      }
 
-    if (document.forms[0].fname_1.value == "") {
-        alert('Пожалуйста, введите Номер карты');
-        document.card_form.fname_1.focus();
-        return false
-    }
+      if (document.forms[0].fname_2.value == "") {
+          alert('Пожалуйста, введите Номер карты');
+          document.card_form.fname_2.focus();
+          return false
+      }
 
-    if (document.forms[0].fname_2.value == "") {
-        alert('Пожалуйста, введите Номер карты');
-        document.card_form.fname_2.focus();
-        return false
-    }
+      if (document.forms[0].fname_3.value == "") {
+          alert('Пожалуйста, введите Номер карты');
+          document.card_form.fname_3.focus();
+          return false
+      }
 
-    if (document.forms[0].fname_3.value == "") {
-        alert('Пожалуйста, введите Номер карты');
-        document.card_form.fname_3.focus();
-        return false
-    }
+      if (document.forms[0].card_name.value == "") {
+          alert('Пожалуйста, введите Держателя карты');
+          document.card_form.card_name.focus();
+          return false
+      }
 
-    if (document.forms[0].card_name.value == "") {
-        alert('Пожалуйста, введите Держателя карты');
-        document.card_form.card_name.focus();
-        return false
-    }
+      if (document.forms[0].CVC.value == "") {
+          alert('Пожалуйста, введите код CVV/CVC2');
+          document.card_form.CVC.focus();
+          return false
+      }
 
-    if (document.forms[0].CVC.value == "") {
-        alert('Пожалуйста, введите код CVV/CVC2');
-        document.card_form.CVC.focus();
-        return false
-    }
-
-    return true;
+      return true;
 }
 
