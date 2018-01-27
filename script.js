a07c1ab5ae757cef5a10c5da39a0389f7c834222
Vadim Leukhin <leuxinvadim@gmail.com>
2018-01-27 14:42:56 +0300

    выполнено обязательное заполнение полей

--- script.js
+++ script.js
@@ -20,6 +20,13 @@ function OnChangeOwner(input) {
         return alert(`Введите больше ${maxLen} символов`);
 }
 
+// Проверяет длину введенных  для CVC кода
+function OnChangeCVC(input) {
+    var maxLen = 3;
+    if (input.value.length < maxLen)
+        return alert('Введите 3 символа');
+}
+
 $('body').on('click keypress', '.code_CVC', function(key) {
     $this = $(this);
     $maxLen = 2;
@@ -30,3 +37,46 @@ $('body').on('click keypress', '.code_CVC', function(key) {
     if (key.charCode < 48 || key.charCode > 57)
         return false;
 });
+
+
+function sendform() {
+
+    if (document.forms[0].fname.value == "") {
+        alert('Пожалуйста, введите Номер карты');
+        document.card_form.fname.focus();
+        return false
+    }
+
+    if (document.forms[0].fname_1.value == "") {
+        alert('Пожалуйста, введите Номер карты');
+        document.card_form.fname_1.focus();
+        return false
+    }
+
+    if (document.forms[0].fname_2.value == "") {
+        alert('Пожалуйста, введите Номер карты');
+        document.card_form.fname_2.focus();
+        return false
+    }
+
+    if (document.forms[0].fname_3.value == "") {
+        alert('Пожалуйста, введите Номер карты');
+        document.card_form.fname_3.focus();
+        return false
+    }
+
+    if (document.forms[0].card_name.value == "") {
+        alert('Пожалуйста, введите Держателя карты');
+        document.card_form.card_name.focus();
+        return false
+    }
+
+    if (document.forms[0].CVC.value == "") {
+        alert('Пожалуйста, введите код CVV/CVC2');
+        document.card_form.CVC.focus();
+        return false
+    }
+
+    return true;
+}
+
